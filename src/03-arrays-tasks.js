29ef305f94a184534252eb457a33f4712bfcc0f7
SPHsofi <109211098+SPHsofi@users.noreply.github.com>
2024-09-02 14:25:56 +0300

    feat: solved 6 array task (removeFalsyValues)

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -103,8 +103,13 @@ function getArrayOfStrings(arr) {
  *    [ 1, 2, 3, 4, 5, 'false' ]         => [ 1, 2, 3, 4, 5, 'false' ]
  *    [ false, 0, NaN, '', undefined ]   => [ ]
  */
-function removeFalsyValues(/* arr */) {
-  throw new Error('Not implemented');
+function removeFalsyValues(arr) {
+  // eslint-disable-next-line array-callback-return,consistent-return
+  return arr.filter((el) => {
+    if (el) {
+      return el;
+    }
+  });
 }
 
 /**
