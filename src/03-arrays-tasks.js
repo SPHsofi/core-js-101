a9e85b248c2fe4bb522c9facb3fd700df17918b6
SPHsofi <109211098+SPHsofi@users.noreply.github.com>
2024-09-05 21:15:58 +0300

    feat: solved 21 array task (getFalsyValuesCount)

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -377,10 +377,9 @@ function getItemsSum(arr) {
  *  [ -1, 'false', null, 0 ] => 2
  *  [ null, undefined, NaN, false, 0, '' ]  => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  return (arr.filter((el) => !el)).length;
 }
-
 /**
  * Returns a number of all occurrences of the specified item in an array
  *
